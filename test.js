c1ebdf1ca03bbd649832bbe028598d465ee0987b
MtrdHntr <sammmpo@gmail.com>
2018-09-13 17:36:26 +0300

    Toinen kurssikerta

---
+++ test.js
@@ -0,0 +1,5 @@
+var a = 5
+var b = 5
+var c = a + b
+
+console.log("5 + 5 =", c)
---
+++ test2.js
@@ -0,0 +1,10 @@
+var a = 1
+var b = 1
+var c = a + b
+var d = 3
+var e = 2
+var f = d - e
+var g = d % e
+
+console.log("1 + 1 =", c)
+console.log("3 - 2 =", f)
